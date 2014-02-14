274ed9490ea14065517003cfbde665298f0a6e53
Robert Yang <yang.robert.w@gmail.com>
2014-02-13 19:22:34 -0500

    added week 3 (physics stuff, intro to code)

---
+++ week03/Assets/scripts/HelloWorld.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	string currentRoom = "Lobby";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ( "Hello World!" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// a "buffer" is a temporary place where we assemble data before displaying it, e.g. a graphics buffer
+		string textBuffer = "TEXT ADVENTURE: You are in the " + currentRoom;
+
+		if ( currentRoom == "Lobby" ) 
+		{
+			// remember, "\n" means "new line"
+			textBuffer += "\nThe NYU Poly security guard stares at you.";
+			textBuffer += "\nPress [W] to go to the elevators.";
+
+			if ( Input.GetKeyDown (KeyCode.W) )
+			{
+				currentRoom = "Elevators";
+			}
+		} 
+		else if ( currentRoom == "Elevators" )
+		{
+			textBuffer += "\nThere are SO MANY ELEVATORS. A safe monkey winks at you.";
+		}
+
+		// overwrite contents of "text" variable on the TextMesh component with contents of textBuffer variable
+		GetComponent<TextMesh>().text = textBuffer;
+	}
+}
