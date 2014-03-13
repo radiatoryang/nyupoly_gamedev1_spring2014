568a07fdefbe665595e0b4d1369eb29e4d1dbe68
Robert Yang <yang.robert.w@gmail.com>
2014-03-13 13:33:38 -0400

    added week 7: level loads for menus (title screen, instructions?) and static variables

---
+++ week07/Assets/scripts/HighScore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+	// a STATIC variable lives in memory, in the type itself
+	// in Unity, this lets us persist data between new level loads
+	public static int score = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		// a great game mechanic
+		if (Input.GetKeyDown (KeyCode.Space))
+			score++;
+			
+		// a simple restart button
+		if (Input.GetKeyDown (KeyCode.R))
+			Application.LoadLevel (2);
+			
+		// display my current score
+		GetComponent<TextMesh>().text = score.ToString();
+	}
+}
---
+++ week07/Assets/scripts/MenuInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuInput : MonoBehaviour {
+
+	public KeyCode keyToPush; // assign in inspector
+	public string levelToLoad; // assign in inspector
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (keyToPush)) {
+			Application.LoadLevel ( levelToLoad );	
+		}
+		
+		// this is accessing a PUBLIC STATIC variable from HighScore.cs
+		Debug.Log (HighScore.score);
+	}
+}
